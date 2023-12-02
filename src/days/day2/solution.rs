a010148df55acd7722ba34a9ad3967eede7661b5
Breno Caldeira Nascimento <breno.cn@hotmail.com>
2023-12-02 14:22:33 -0300

    refactor day 2 to use struct methods

--- src/days/day2/solution.rs
+++ src/days/day2/solution.rs
@@ -1,10 +1,26 @@
-use std::{collections::HashMap, fs};
+use std::fs;
 
 #[derive(Debug)]
 struct Game {
     reds: Vec<i32>,
     greens: Vec<i32>,
     blues: Vec<i32>,
+}
+
+impl Game {
+    pub fn is_possible(&self, red: i32, green: i32, blue: i32) -> bool {
+        self.reds.iter().all(|ammount| *ammount <= red)
+            && self.greens.iter().all(|ammount| *ammount <= green)
+            && self.blues.iter().all(|ammount| *ammount <= blue)
+    }
+
+    pub fn fewest_possible(&self) -> (i32, i32, i32) {
+        let red = self.reds.iter().max().unwrap();
+        let green = self.greens.iter().max().unwrap();
+        let blue = self.blues.iter().max().unwrap();
+
+        (*red, *green, *blue)
+    }
 }
 
 fn process_game(game: &Vec<&str>) -> Game {
@@ -51,12 +67,6 @@ fn get_input() -> Vec<Game> {
     games
 }
 
-fn is_game_possible(red: i32, green: i32, blue: i32, game: &Game) -> bool {
-    game.reds.iter().all(|ammount| *ammount <= red)
-        && game.greens.iter().all(|ammount| *ammount <= green)
-        && game.blues.iter().all(|ammount| *ammount <= blue)
-}
-
 pub fn part1() -> i32 {
     let games = get_input();
     let red_target = 12;
@@ -66,17 +76,10 @@ pub fn part1() -> i32 {
     games
         .iter()
         .enumerate()
-        .filter(|(_, game)| is_game_possible(red_target, green_target, blue_target, game))
+        // .filter(|(_, game)| is_game_possible(red_target, green_target, blue_target, game))
+        .filter(|(_, game)| game.is_possible(red_target, green_target, blue_target))
         .map(|(index, _)| (index + 1) as i32)
         .sum()
-}
-
-fn fewest_possible(game: &Game) -> (i32, i32, i32) {
-    let red = game.reds.iter().max().unwrap();
-    let green = game.greens.iter().max().unwrap();
-    let blue = game.blues.iter().max().unwrap();
-
-    (*red, *green, *blue)
 }
 
 pub fn part2() -> i32 {
@@ -84,7 +87,7 @@ pub fn part2() -> i32 {
 
     games
         .iter()
-        .map(fewest_possible)
+        .map(Game::fewest_possible)
         .map(|(red, green, blue)| red * green * blue)
         .sum()
 }
